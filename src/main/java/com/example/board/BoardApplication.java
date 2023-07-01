2621cc5e9564c19abcf886af2a132c82df44e604
kwagi <heisenberg123457@gmail.com>
2023-07-01 20:04:57 +0900

    cors config

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -3,11 +3,24 @@ package com.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
